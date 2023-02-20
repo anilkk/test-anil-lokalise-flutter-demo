34416450d1a95b2cee99fd15eca654cda91c068a
Anil Kumar <anil.kumar@lokalise.com>
2023-02-20 10:23:07 +0100

    add Lokalise flutter SDK gudei

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:lokalise_flutter_sdk/lokalise_flutter_sdk.dart';
+import 'package:to_do_app/app/app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Lokalise.init(
+    projectId: '', // Fill with your project id
+    sdkToken: '', // Fill with your SDK token
+  );
+  runApp(const App());
+}
